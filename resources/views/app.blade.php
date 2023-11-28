49be6a4a9adbd2250b9248c0047884bdaee85cfe
LucaCGN <caco.nonino@outlook.com.br>
2023-11-28 14:47:08 -0300

    adjustments

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,32 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Datagro Comercial Team Web Application</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Datagro Comercial Team Web Application</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <!-- Header content goes here -->
-    </header>
+  <header>
+      <!-- Header content goes here -->
+  </header>
 
-    <main>
-        <!-- Include the updated Blade templates for each component -->
-        @include('partials.dropdown-filter')
-        @include('partials.products-table')
-        @include('partials.data-series-table')
-        @include('partials.download-buttons')
-    </main>
+  <main>
+      <!-- Main content goes here -->
+  </main>
 
-    <footer>
-        <!-- Footer content goes here -->
-    </footer>
+  <footer>
+      <!-- Footer content goes here -->
+  </footer>
 
-    <!-- Include the JavaScript files -->
-    <script src="{{ asset('js/dropdown-filter.js') }}"></script>
-    <script src="{{ asset('js/products-table.js') }}"></script>
-    <script src="{{ asset('js/data-series.js') }}"></script>
-    <script src="{{ asset('js/download-buttons.js') }}"></script>
+  <!-- Include the JavaScript files -->
+  <script type="module" src="{{ asset('js/app.js') }}"></script>
+  <script type="module" src="{{ asset('js/DropdownFilter.js') }}"></script>
+  <script type="module" src="{{ asset('js/ProductsTable.js') }}"></script>
+  <script type="module" src="{{ asset('js/DataSeriesTable.js') }}"></script>
+  <script type="module" src="{{ asset('js/DownloadButtons.js') }}"></script>
 </body>
 </html>
