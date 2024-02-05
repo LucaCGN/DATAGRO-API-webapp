9935b3745ffc5c3bb90f99460b4ba0539154bfc0
LucaCGN <caco.nonino@outlook.com.br>
2024-02-05 14:21:22 -0300

    Changed Routes

--- app/Http/Controllers/LuigiPipelineController.php
+++ app/Http/Controllers/LuigiPipelineController.php
@@ -8,25 +8,25 @@ class LuigiPipelineController extends Controller
 {
     public function triggerUSDA()
     {
-        exec("python3 /path/to/your/luigi_app/main.py --pipeline USDA");
+        exec("python3 ./luigi/main.py --pipeline USDA");
         return response()->json(['message' => 'USDA pipeline triggered successfully.'], 200);
     }
 
     public function triggerCOMEX()
     {
-        exec("python3 /path/to/your/luigi_app/main.py --pipeline COMEX");
+        exec("python3 ./luigi/main.py --pipeline COMEX");
         return response()->json(['message' => 'COMEX pipeline triggered successfully.'], 200);
     }
 
     public function triggerINDEC()
     {
-        exec("python3 /path/to/your/luigi_app/main.py --pipeline INDEC");
+        exec("python3 ./luigi/main.py --pipeline INDEC");
         return response()->json(['message' => 'INDEC pipeline triggered successfully.'], 200);
     }
 
     public function triggerAllPipelines()
     {
-        exec("python3 /path/to/your/luigi_app/main.py --pipeline ALL");
+        exec("python3 ./luigi/main.py --pipeline ALL");
         return response()->json(['message' => 'All pipelines triggered successfully.'], 200);
     }
 }
