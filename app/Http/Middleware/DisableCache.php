4e740f1d1e377fd39dde94646313d7470b6d09a8
LucaCGN <caco.nonino@outlook.com.br>
2024-02-09 15:20:32 -0300

    Updated DIsableCache Middleware

--- app/Http/Middleware/DisableCache.php
+++ app/Http/Middleware/DisableCache.php
@@ -3,16 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DisableCache
 {
-   public function handle($request, Closure $next)
-   {
-       $response = $next($request);
-       $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate');
-       $response->header('Pragma', 'no-cache');
-       $response->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
 
-       return $response;
-   }
+        // Check if the response is not a BinaryFileResponse
+        if (!$response instanceof BinaryFileResponse) {
+            $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate');
+            $response->header('Pragma', 'no-cache');
+            $response->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        }
+
+        return $response;
+    }
 }
